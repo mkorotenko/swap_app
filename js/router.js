522ca1bc84a7353ba551ac51430bdcd4210c127f
Maxim <m.v.korotenko@gmail.com>
2016-05-12 19:33:43 +0300

    Table styling #1

--- js/router.js
+++ js/router.js
@@ -4,10 +4,10 @@ define([
   'underscore',
   'backbone',
   'app/view/home'
-], function($, _, Backbone, HomeView){
+], function($, _, Backbone, Pages){
   var $content = $("#header"),
       $main = $("#data-container"),
-      homeView = new HomeView({el: $content});
+      pages = new Pages({el: $content});
   var AppRouter = Backbone.Router.extend({
     routes: {
       '' : 'accounts',
@@ -23,45 +23,18 @@ define([
       }
     },
     accounts: function() {
-      // homeView.render();
-      homeView.accounts();
+      pages.accounts();
       this.renderPath([]);
     },
     transactions: function(account) {
-      // console.log('Account id: '+account);
-      homeView.transactions(account);
+      pages.transactions(account);
       this.renderPath([account]);
-      // var currentPath = $('#current-path');
-      // currentPath.html();
-      // currentPath.append('<a href="#/">Account list</a>');
-      // currentPath.append('<span>/</span>');
-      // currentPath.append('<a href="#/card/'+account+'">'+account+'</a>');
     }
       // setFilter: function(params) {
       //   console.log('app.router.params = ' + params);
       //   window.filter = params.trim() || '';
       //   app.accList.trigger('reset');
       // },
-      // newRecord: function(params) {
-      //   console.log('New record = ' + params);
-      // },
-      // corRecord: function(params) {
-      //   console.log('Cor record = ' + params);
-      // },
-      // transactions: function(category) {
-      //   //console.log('Transactions = ' + category);
-      //   app.appView.transactionsPage(category);
-      // },
-      // editRecord: function(recId) {
-      //   if(recId == 'new'){
-          
-      //   }
-      //   else
-      //   {
-      //     var mod = app.transactionList.get(recId);
-      //     app.appView.editModel(mod);
-      //   }
-      // }
   });
 
   return AppRouter;
