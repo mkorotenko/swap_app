5ee41104eb6e2d2ab70c0e5abf1c7eb13f14244a
Maxim <m.v.korotenko@gmail.com>
2016-05-16 07:54:42 +0300

    Restfull #2

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ define([
     switchPage: function(page,unitId){
       require(['view/'+page+'ListView','app/model/'+page],function(ListPageView,Collection){
         var view = new ListPageView({
-          collection: new (Collection.extend({category:'usd'}))()
+          collection: new (Collection.extend({category:unitId}))()
         });
         view.open(unitId).update();
       }.bind(this));
