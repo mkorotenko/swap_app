e8b1d9d5125d3139fd77598fce56da74ee9b6fed
Maxim <m.v.korotenko@gmail.com>
2016-05-21 23:03:43 +0300

    Transaction page #3

---
+++ js/app/view/transactionView.js
@@ -0,0 +1,35 @@
+define(function (require) {
+
+  "use strict";
+
+  var $                   = require('jquery'),
+      _                   = require('underscore'),
+      Backbone            = require('backbone'),
+      ListPageView        = require('view/listPage'),
+      tpl                 = require('text!tpl/transactionPage.html');
+
+  return ListPageView.extend({
+    tagName: 'div',
+    className: 'table-row-box',
+    template: _.template(tpl),
+    initialize: function(){
+      this.render();
+      return this;
+    },
+    open: function() {
+      this.render();
+      return this;
+    },
+    update: function() {
+      this.busy(false);
+      this.$el.html(this.template(this.model.toJSON()));
+      $('#account-list').append(this.$el);
+      return this;
+    },
+    render: function(){
+      this.constructor.__super__.render.apply(this,arguments);
+      return this;
+    }
+  });
+
+});
