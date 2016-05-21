f1dffaf5a1e43e6a0a607093938d09b6245487f8
Maxim <m.v.korotenko@gmail.com>
2016-05-21 23:40:48 +0300

    Path #5

--- js/app/view/listPage.js
+++ js/app/view/listPage.js
@@ -12,12 +12,14 @@ define(function (require) {
     options: {path: []},
     renderPath: function() {
       var currentPath = $('#current-path'),
+          pathStr = '',
           path = this.path || this.options.path || [];
       currentPath.html('');
       currentPath.append('<a href="#">Account list</a>');
       for(var i=0; i<path.length; i++){
         currentPath.append('<span>/</span>');
-        currentPath.append('<a href="#card/'+path[i]+'">'+path[i]+'</a>');
+        pathStr += '/'+path[i];
+        currentPath.append('<a href="#card'+pathStr+'">'+path[i]+'</a>');
       }
       return this;
     },
